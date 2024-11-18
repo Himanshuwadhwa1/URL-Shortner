9b71a66b01342171ec94a6fffe1837ab018f03af
Himanshu wadhwa <himanshuhw999@gmail.com>
2024-11-18 21:03:19 +0530

    configuration done

--- server/src/config/dbConfig.ts
+++ server/src/config/dbConfig.ts
@@ -3,10 +3,7 @@ import mongoose from "mongoose";
 const connectDB =async ()=>{
     try{
         const connect = await mongoose.connect(`${process.env.CONNECTION_STRING}`)
-        console.log(`MongoDB connected: ,
-            ${connect.connection.host},
-            ${connect.connection.name}`
-        )
+        console.log('MongoDB connected')
     }catch(err){
         console.log(err);
     }
