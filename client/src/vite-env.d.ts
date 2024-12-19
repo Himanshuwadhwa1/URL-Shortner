3ae8e03a307d0ba8271dd8e3886d156bfb4acd90
Himanshu wadhwa <himanshuhw999@gmail.com>
2024-12-19 21:15:47 +0530

    env variable

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+// interface ImportMetaEnv {
+//     readonly VITE_SERVER_URL: string;
+//     // You can add more environment variables here           //to do
+//   }
+  
+//   interface ImportMeta {
+//     readonly env: ImportMetaEnv;
+//   }
