a1ded87695e83b2e5b1b15b4fda82bc7a4544e80
Himanshu wadhwa <himanshuhw999@gmail.com>
2024-12-20 00:10:49 +0530

    ready

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,10 +10,13 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+const allowedOrigins = ['https://url-shortner-pi.vercel.app'];
+
 app.use(cors({
-    origin: "https://url-shortner-pi.vercel.app/",
-    credentials:true,
-}))
+    origin: allowedOrigins,
+    methods: ['GET', 'POST'],
+    credentials: true,
+}));
 app.use('/api',router);
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`)
